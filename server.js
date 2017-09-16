e8c731d879abe02fbc33533432f6331e5469e41d
Phelipe Mizerani <phelipemizerani@gmail.com>
2017-09-16 16:01:54 -0300

    Remove Project

--- server.js
+++
@@ -1,8 +0,0 @@
-//includes
-let app = require("./config/express")();
-let http = require('http').createServer(app);
-
-//create server node
-http.listen(app.get('port'), () => {
-    console.log('Application listen on port: ' + app.get('port'));
-});
